f74d3ef1236422fdc167a97d63a1c30935617610
Fajar-Islami <ahmadfajarislami@protonmail.com>
2020-12-22 22:05:17 +0700

    VIdeo 4 install ejs

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const app = express();
 
+// EJS
+app.set('view engine','ejs')
+
 app.listen(9090, console.log('Server running on PORT 9090'));
 
 app.get('/', (req, res) => {
-  res.sendFile('./views/index.html', { root: __dirname }); //params kedua root direktori
+  // res.sendFile('./views/index.html', { root: __dirname }); //params kedua root direktori
+  // ejs jadi berubah
+  res.render('index')
 });
 
 app.get('/add-item', (req, res) => {
-  res.sendFile('./views/add-item.html', { root: __dirname });
+  res.render('add-item')
 });
 
 // 404 file => harus tarok di paling bawah
 app.use((req, res) => {
-  res.sendFile('./views/404.html', { root: __dirname });
+  res.render('404')
 });
